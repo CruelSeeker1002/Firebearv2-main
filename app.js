cf0c6d53180fac589bcfc393cc5d0155ef8a149c
Cruelseeker1002 <awesomeruggy@gmail.com>
2021-11-11 19:08:30 +0530

    new sign in/log in page

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const port = 8001;
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/html/index.html");
+});
+app.get("/login", function(req, res) {
+    res.sendFile(__dirname + "/public/html/login.html");
+});
+app.get("/aboutus", function(req, res) {
+    res.sendFile(__dirname + "/public/html/aboutus.html");
+});
+app.get("/register", function(req, res) {
+    res.sendFile(__dirname + "/public/html/register.html");
+});
+
+app.listen(process.env.PORT || port, function() {
+    console.log(`Server started on http://localhost:${port}`);
+});
