8bb2b119b7b31c300460b9772bef429f185ebda9
Cruelseeker1002 <awesomeruggy@gmail.com>
2021-11-14 18:24:13 +0530

    changed some js code

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,51 @@
-function login (){
-    var userEmail = document.getElementById("email_field").value;
-    var userPassword = document.getElementById("password_field").value;
-    window.alert(userEmail + "" + userPassword);
+function signUp() {
+
+    var name = $('#signupName').val();
+    var email = $('#signupEmail').val();
+    var password = $('#signupPassword').val();
+
+    if (!(name === "") && !(email === "") && !(password === "")) {
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = firebase.auth().currentUser;
+
+            user.updateProfile({
+            displayName: name
+            }).then(() => {
+
+                firebase.auth().currentUser.sendEmailVerification()
+                .then(() => {
+
+                    Swal.fire({
+                        title: 'Success!',
+                        text: "Signed up successfully! Check your inbox for the verification email",
+                        icon: 'success',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'Okay'
+                      }).then((result) => {
+                        if (result.isConfirmed) {
+                          window.location = "/"
+                        }
+                    })
+                });
+                
+            }).catch((error) => {
+                Swal.fire(
+                    'Error!',
+                    error.message,
+                    'error'
+                )
+            });  
+        })
+        .catch((error) => {
+            Swal.fire(
+                'Error!',
+                error.message,
+                'error'
+            )
+        });
+
+    }
+
 }
