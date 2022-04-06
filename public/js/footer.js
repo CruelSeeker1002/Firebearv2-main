ef222677be34e8a9b8cfba99f810945230376487
Cruelseeker1002 <awesomeruggy@gmail.com>
2022-04-06 22:24:22 +0530

    removed about us and changed footer

--- public/js/footer.js
+++ public/js/footer.js
@@ -34,7 +34,7 @@ const createFooter = () => {
         </div>
     </div>
     <p class="footer-title">about company</p>
-    <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
+    <p class="info">The company supports all rights and is in no way affliated with any other company that is racist or homophobic. Here at Firebear we have your treatment in mind and promise to always treat our customers right and give refunds if the product does not come to your liking. All we ask in return is the reciept and it to not be used.Terms and conditions apply. </p>
     <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
     <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
     <div class="footer-social-container">
